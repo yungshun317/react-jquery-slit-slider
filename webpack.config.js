04238075ce5743ae3365d9f231f7d1652b22b783
yungshun317 <seraph317@gmail.com>
2021-06-29 13:36:14 +0800

    [CHORE] Configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+const path = require("path");
+
+module.exports = {
+    entry: {
+        index: path.resolve(__dirname, "src", "index.js")
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+        sourceMapFilename: "bundle.map"
+    },
+    devtool: "source-map",
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        port: 9000,
+        open: "google-chrome"
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: ["style-loader", "css-loader"]
+        }, {
+            test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+            use: ["url-loader"]
+
+        }, {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: ["babel-loader"]
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "public", "index.html")
+        }),
+        new webpack.ProvidePlugin({
+            "$": "jquery",
+            "jQuery": "jquery",
+            "window.jQuery": "jquery"
+        })
+    ]
+}
