0ee22d372e7a62a344498e3ee79c4c38ec7a13c7
yungshun317 <seraph317@gmail.com>
2021-07-09 10:30:59 +0800

    [FEAT] Create the `SlitSlider` component

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import SlitSlider from "./SlitSlider";
+
+export default function App() {
+    return <SlitSlider />
+}
---
+++ src/SlitSlider.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import $ from "jquery";
+
+export default function SlitSlider() {
+    return (
+        <></>
+    );
+}
