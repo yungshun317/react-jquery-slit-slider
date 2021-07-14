d1fdbf44928bae6ec63adc1f908402bae793d0e9
yungshun317 <seraph317@gmail.com>
2021-07-14 17:21:05 +0800

    [FEAT] Set the second slide

--- src/SlitSlider.js
+++ src/SlitSlider.js
@@ -12,17 +12,31 @@ export default function SlitSlider() {
                  data-slice1-scale="2"
                  data-slice2-scale="2">
                 <div class="sl-slide-inner">
-                    <div class="sl-slide-inner">
-                        <div class="deco" data-icon="H"></div>
-                        <h2>A bene placito</h2>
-                        <blockquote>
-                            <p>You have just dined, and however scrupulously the slaughterhouse is concealed in
+                    <div class="deco" data-icon="H"></div>
+                    <h2>A bene placito</h2>
+                    <blockquote>
+                        <p>You have just dined, and however scrupulously the slaughterhouse is concealed in
                             the graceful distance of miles, there is complicity.</p>
-                            <cite>Ralph Waldo Emerson</cite>
-                        </blockquote>
-                    </div>
+                        <cite>Ralph Waldo Emerson</cite>
+                    </blockquote>
                 </div>
+            </div>
 
+            <div className="sl-slide bg-2"
+                 data-orientation="vertical"
+                 data-slice1-rotation="10"
+                 data-slice2-rotation="-15"
+                 data-slice1-scale="1.5"
+                 data-slice2-scale="1.5">
+                <div className="sl-slide-inner">
+                    <div className="deco" data-icon="q"></div>
+                    <h2>Regula aurea</h2>
+                    <blockquote>
+                        <p>Until he extends the circle of his compassion to all living things, man will not
+                            himself find peace.</p>
+                        <cite>Albert Schweitzer</cite>
+                    </blockquote>
+                </div>
             </div>
         </>
     );
