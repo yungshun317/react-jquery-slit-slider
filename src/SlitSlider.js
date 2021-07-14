0b12f47fab660c862a48695897bf0e28578dd25f
yungshun317 <seraph317@gmail.com>
2021-07-14 17:17:12 +0800

    [FEAT] Set the first slide

--- src/SlitSlider.js
+++ src/SlitSlider.js
@@ -4,6 +4,26 @@ import $ from "jquery";
 
 export default function SlitSlider() {
     return (
-        <></>
+        <>
+            <div class="sl-slide bg-1"
+                 data-orientation="horizontal"
+                 data-slice1-rotation="-25"
+                 data-slice2-rotation="-25"
+                 data-slice1-scale="2"
+                 data-slice2-scale="2">
+                <div class="sl-slide-inner">
+                    <div class="sl-slide-inner">
+                        <div class="deco" data-icon="H"></div>
+                        <h2>A bene placito</h2>
+                        <blockquote>
+                            <p>You have just dined, and however scrupulously the slaughterhouse is concealed in
+                            the graceful distance of miles, there is complicity.</p>
+                            <cite>Ralph Waldo Emerson</cite>
+                        </blockquote>
+                    </div>
+                </div>
+
+            </div>
+        </>
     );
 }
